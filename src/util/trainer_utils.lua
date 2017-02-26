a1792c30203fe832cdd5af40614da590e25208d0
YuzhongHuang <yuzhong.huang@students.olin.edu>
2017-02-26 14:56:00 -0500

    clean up some scafolding code in trainer_utils.lua

--- src/util/trainer_utils.lua
+++ src/util/trainer_utils.lua
@@ -12,27 +12,22 @@ require "./data_utils"
 
 
 function train(iterations, learningRate, learningDecay, batchSize, frameNum, imgSize, model, criterion, trainPath, testPath, videoPath)
+    -- encoding high level parameters
     local optimState = {learningRate=learningRate, learningDecay=learningDecay, momentum = 0.5}
 
-    -- call data_util.lua to generate test data
-    -- ****Comment this block out for full dataset training*****
+    -- call getTest() to generate test data
     local testset = getTest(testPath, videoPath, frameNum, batchSize, imgSize)
-    local batchInputs, batchLabels = getBatch(trainPath, videoPath, batchSize, frameNum, imgSize)
-    -- ****Comment this block out for full dataset training*****
 
     -- initialize an accuracy table to write down accuracies in each epoch 
-    local accy_table = {}
+    local accuracy_table = {}
 	
     for epoch=1,iterations do
-        print('Epoch '..epoch)
+        print('Current Epoch: '..epoch)
 
         local params, gradParams = model:getParameters()
 
-        -- call data_util.lua to generate batchInputs and batchLabels
-        
-        -- ****UnComment this block out for full dataset training*****
-        --local batchInputs, batchLabels = getBatch(trainPath, videoPath, batchSize, frameNum, imgSize)
-	-- ****UnComment this block out for full dataset training*****
+        -- call getBatch() to generate batchInputs and batchLabels
+        local batchInputs, batchLabels = getBatch(trainPath, videoPath, batchSize, frameNum, imgSize)
 
         local function feval(params)
             gradParams:zero()
@@ -40,14 +35,10 @@ function train(iterations, learningRate, learningDecay, batchSize, frameNum, img
             local outputs = model:forward(batchInputs)
             local loss = criterion:forward(outputs, batchLabels)
             print('Train Error '..loss)
-            
-	    -- ****UnComment this block out for full dataset training*****
-            --local testset = getTest(testPath, videoPath, frameNum, batchSize, imgSize)
-            -- ****UnComment this block out for full dataset training*****
 
-            local accy = accuracy(model, testset)
-            print("Test Accuracy "..accy)
-            accy_table[epoch] = accy		
+            local accuracy = accuracy(model, testset)
+            print("Test Accuracy "..accuracy)
+            accuracy_table[epoch] = accuracy		
 
             local dloss_doutput = criterion:backward(outputs, batchLabels)
             model:backward(batchInputs, dloss_doutput)
@@ -58,18 +49,16 @@ function train(iterations, learningRate, learningDecay, batchSize, frameNum, img
         optim.sgd(feval, params, optimState)
 
 		-- call train_util.lua to compute the test accuracy
-		-- local a = accuracy(model, testset)
-		-- print('Test Accuracy '..a)
+		local a = accuracy(model, testset)
+		print('Test Accuracy '..a)
 
         model:zeroGradParameters() 
         model:forget()
     
-        torch.save('accuracy.t7', accy_table)
-
+        torch.save('accuracy.t7', accuracy_table)
     end
 
     -- save the model in the end of the training
     model:clearState()
     torch.save('model.t7', model)
-    torch.save('accuracy.t7', accy_table)
 end
