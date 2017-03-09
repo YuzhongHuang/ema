3a3c1136dca9a5db88cdb6444345e2f594662ff6
YuzhongHuang <yuzhong.huang@students.olin.edu>
2017-03-08 19:14:06 -0500

    print the accuracy for testing purposes

--- src/experiment/kth/train.lua
+++ src/experiment/kth/train.lua
@@ -79,4 +79,4 @@ end
 -- call training function
 trained_model = train(optimState, opt, trainset, net, criterion)
 -- test trained model with test dataset
-accuracy(trained_model, getTest(testPath, videoPath, frameNum, imgSize, testBatchTotal, testName))
+print(accuracy(trained_model, getTest(testPath, videoPath, frameNum, imgSize, testBatchTotal, testName)))
