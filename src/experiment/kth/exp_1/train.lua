b55c6409103fdef32abc3731b7df597ffd299a93
YuzhongHuang <yuzhong.huang@students.olin.edu>
2017-07-26 22:51:03 +0000

    ignore ucf data

--- src/experiment/kth/exp_1/train.lua
+++ src/experiment/kth/exp_1/train.lua
@@ -19,7 +19,7 @@ require "../../../util/networks"
 -- configurations
 gpuFlag = true  -- set running mode
 imgSize = 32
-gpus = {1,2,3,4,5,6,7,8}
+gpus = {1}
 
 -- data loading path
 trainPath = "../../../../kthData/split1/train"
