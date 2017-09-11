0ebd0177a43701d4e0156c72616535b7791c52e6
YuzhongHuang <yuzhong.huang@students.olin.edu>
2017-09-11 22:23:24 +0000

    implementing get_weight(haven't tested)

--- src/util_all/plot_utils.lua
+++ src/util_all/plot_utils.lua
@@ -1,4 +1,6 @@
 require "gnuplot"
+require "lfs"
+require "image"
 
 -- a function that plots the overall accuracy
 function plot(name, iter, a, e)
@@ -19,11 +21,14 @@ end
 -- get_weight() takes a network with a given indices to a layer, 
 -- and a video input. Generate folders of images outputs of that layer's 
 -- weights and activation to given location with a given filename
+
+-- vid should be in the form of (1, frameNum, channelNum, imgSize, imgSize)
+-- model will need
 function get_weight(indices, model, vid, filename, save_path)
 	-- potential bug: need to call Data Paralell
 
 	-- forward pass the video to the network
-	model:forward(vid)
+	model:forward(vid:cuda())  -- :cuda() needed for gpu-trained networks
 
 	-- get to the layer
 	local layer = model
@@ -36,5 +41,11 @@ function get_weight(indices, model, vid, filename, save_path)
 	local outputs = layer.output
 
 	-- save the activation images
-
+	for i = 1, (#outputs)[2] do -- loop through channels
+	    vid_path = save_path.."/"..filename.."_channel"..i
+	    lfs.mkdir(vid_path)
+	    for j = 1, (#outputs)[1] do
+		image.save(vid_path.."img_naming"..".png", outputs[j][i])	
+	    end
+	end
 end
