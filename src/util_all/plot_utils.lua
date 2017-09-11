9335ce2a8b0fe76ca10b7e879d0c69a900a89811
YuzhongHuang <yuzhong.huang@students.olin.edu>
2017-09-11 17:23:53 -0400

    get_weight function(haven't tested)

--- src/util_all/plot_utils.lua
+++ src/util_all/plot_utils.lua
@@ -15,3 +15,26 @@ function plot(name, iter, a, e)
 
 	gnuplot.plotflush()
 end
+
+-- get_weight() takes a network with a given indices to a layer, 
+-- and a video input. Generate folders of images outputs of that layer's 
+-- weights and activation to given location with a given filename
+function get_weight(indices, model, vid, filename, save_path)
+	-- potential bug: need to call Data Paralell
+
+	-- forward pass the video to the network
+	model:forward(vid)
+
+	-- get to the layer
+	local layer = model
+	for i = 1, #indices do
+		layer = model.modules[indices[i]]
+	end
+
+	-- get the weight and activation
+	local weights = layer.weight
+	local outputs = layer.output
+
+	-- save the activation images
+
+end
